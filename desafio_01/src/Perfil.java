477e445efbb7af76b6b3b7a638de9417922cc0cc
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-04-04 10:45:33 -0300

    criando classe estudos e soma

--- desafio_01/src/Perfil.java
+++ desafio_01/src/Perfil.java
@@ -3,5 +3,17 @@ public class Perfil {
 
         System.out.println("Olá, Marcos Schwaab");
         System.out.println("Tudo bem?");
+
+        new Estudos();
+    }
+}
+
+class Estudos {
+    int numero1 = 10;
+    int numero2 = 5;
+    int resultado = numero1 + numero2;
+
+    Estudos() {
+        System.out.println("A soma é: " + resultado);
     }
 }
