6d0e94c0eca02aad9e6674680180c417077324b0
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-04-04 10:47:13 -0300

    adicionando impressao na tela

--- desafio_01/src/Perfil.java
+++ desafio_01/src/Perfil.java
@@ -4,6 +4,10 @@ public class Perfil {
         System.out.println("Olá, Marcos Schwaab");
         System.out.println("Tudo bem?");
 
+        System.out.println("JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por executar o bytecode");
+        System.out.println("JRE (Java Runtime Environment) - Ambiente de Execução do Java que fornece as bibliotecas padrões do Java para o JDK compilar o seu código e para a JVM executar o seu program");
+
+
         new Estudos();
     }
 }
