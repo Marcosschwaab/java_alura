a24afbab9e97e65bef1524fbeb3e7a4c2c67fb67
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-04-22 13:25:03 -0300

    modulo_02 aula get and setters

--- modulo_02/screenmatch/src/Principal.java
+++ modulo_02/screenmatch/src/Principal.java
@@ -1,19 +1,22 @@
+import br.com.alura.screenmach.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.genero = "Drama";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.incluidoNoPlano = true;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setGenero("Drama");;
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setIncluidoNoPlano(true);
+
 
 
         meuFilme.exibeFichaRTecnica();
 
         meuFilme.avalia(8);
         meuFilme.avalia(5);
+        meuFilme.avalia(10);
 
-        System.out.println (meuFilme.somaDasAvaliacoes);
-        System.out.println (meuFilme.totalDeAvaliacoes);
+        System.out.println ("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println (meuFilme.mediaDasAvaliacoes());
 
     }
---
+++ modulo_02/screenmatch/src/br/com/alura/screenmach/modelos/Filme.java
@@ -0,0 +1,79 @@
+package br.com.alura.screenmach.modelos;
+
+public class Filme {
+    private String nome;
+    private String genero;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double avaliacao;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    }
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+    }
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+
+    public void exibeFichaRTecnica() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Incluido no plano: " + incluidoNoPlano);
+        System.out.println("Avaliação: " + avaliacao);
+        System.out.println("Total de avaliações: " + totalDeAvaliacoes);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+    public double mediaDasAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
+
