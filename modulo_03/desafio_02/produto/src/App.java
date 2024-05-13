0391a7a612c28373b5989dfea7080c2a94ef2e30
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-05-13 16:39:41 -0300

    Modulo_03 desafio_02 completo

---
+++ modulo_03/desafio_02/produto/src/App.java
@@ -0,0 +1,32 @@
+import br.com.alura.desafio.Produto;
+import br.com.alura.desafio.ProdutoPerecivel;
+
+import java.util.ArrayList;
+
+public class App {
+        public static void main(String[] args) {
+        ArrayList<Produto> listaProdutos = new ArrayList<>();
+
+        ProdutoPerecivel produtoPerecivel = new ProdutoPerecivel("Produto C", 12.75, 2, "2023-12-31");
+        System.out.println(produtoPerecivel);
+
+
+        Produto produto1 = new Produto("Sabão", 15.99, 5);
+        Produto produto2 = new Produto("Detergente", 10.49, 8);
+        
+        Produto produtoNovo = new Produto("Novo Produto", 15.99, 3);
+    
+        listaProdutos.add(produto1);
+        listaProdutos.add(produto2);
+        System.out.println(produtoNovo);
+    
+        System.out.println("Tamanho da lista: " + listaProdutos.size());
+        System.out.println("Produto na posição 0: " + listaProdutos.get(0).getNome());
+
+        for (Produto produto : listaProdutos) {
+            System.out.println(produto);
+        }
+    }
+}
+
+
