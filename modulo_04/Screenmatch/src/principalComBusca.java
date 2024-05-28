6aa6cb234dc611cefd0ae30f2454f932e9a3247b
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-05-28 10:31:04 -0300

    Modulo_04 screenmatch biblioteca Gson e uso de Serialized

--- modulo_04/Screenmatch/src/principalComBusca.java
+++ modulo_04/Screenmatch/src/principalComBusca.java
@@ -3,20 +3,36 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class principalComBusca {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("O que deseja pesquisar?");
+        var busca = leitor.nextLine();
+
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=b54ed546";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-             .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=b54ed546"))
+             .uri(URI.create(endereco))
             .build();
 
                 HttpResponse<String> response = client
                          .send(request, HttpResponse.BodyHandlers.ofString());
 
-                System.out.println(response.body());
-            
+                String json = response.body();
+                System.out.println(json);
+
+                Gson gson = new Gson();
+                Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+                System.out.println(meuTitulo);
         }   
 }
