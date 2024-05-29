921dd3a97b5a26520de624baa076d7579571e646
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-05-29 14:46:28 -0300

    Modulo_04 desafios completos

--- modulo_04/desafio_02/pessoa/src/App.java
+++ modulo_04/desafio_02/pessoa/src/App.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
-
+import com.google.gson.GsonBuilder;
+import com.google.gson.Strictness;
 
 import br.com.alura.desafio.Pessoa;
 
@@ -7,7 +8,7 @@ public class App {
     public static void main(String[] args) {
         String json = "{\"nome\":\"João Silva\",\"idade\":25,\"cidade\":\"São Paulo\"}";
 
-        Gson gson = new Gson();
+      Gson gson = new GsonBuilder().setStrictness(Strictness.LENIENT).create();
         Pessoa pessoa = gson.fromJson(json, Pessoa.class);
 
         System.out.println("Objeto: " + pessoa);
