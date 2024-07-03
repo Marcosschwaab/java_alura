cab625d52c4a7176c8685484a167aa3e6b86ee9a
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-07-03 20:43:25 -0300

    criando records medico e endereco

--- api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
+++ api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.med.api.medico.DadosCadastroMedico;
+
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
 
-        System.out.println(json);
+        System.out.println(dados);
     }
 
 }
