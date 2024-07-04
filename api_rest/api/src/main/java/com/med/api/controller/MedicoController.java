7d11b84ed1d05601b05f3ecb99a183a7cc34273f
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-07-04 19:37:41 -0300

    POST funcionando com repositoty

--- api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
+++ api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
@@ -1,8 +1,6 @@
 package com.med.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +12,6 @@ import com.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-
 
 public class MedicoController {
 
