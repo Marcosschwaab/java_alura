98eaf8cea356a29ad9dadd5c6bcc9911f4733306
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-07-04 01:06:30 -0300

    conectando jpa postegres e criando interface e classes

--- api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
+++ api_rest/api/src/main/java/com/med/api/controller/MedicoController.java
@@ -1,20 +1,31 @@
 package com.med.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.med.api.medico.DadosCadastroMedico;
+import com.med.api.medico.Medico;
+import com.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+    
+    
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        medicoRepository.save(new Medico(dados));
 
-        System.out.println(dados);
     }
-
 }
---
+++ api_rest/api/src/main/java/com/med/api/medico/MedicoRepository.java
@@ -0,0 +1,10 @@
+package com.med.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    
+}
